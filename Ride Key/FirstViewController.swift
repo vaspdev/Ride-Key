5c9cd802a4dc0087c1f866224e51f83fa45dbc70
VASP <vasp@VASP-MAC1.local>
2015-01-30 09:56:03 +0530

    Initial Commit

---
+++ Ride Key/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Ride Key
+//
+//  Created by VASP on 30/01/15.
+//  Copyright (c) 2015 VASP. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
